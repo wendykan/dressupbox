ef010a49be6d5a514d81858b03d7bc4e6f0984aa
Sharon Wong <sharonwong@alumni.cmu.edu>
2012-06-30 19:10:14 -0700

    Adding some admin tools and stub views. Removing mysite.

--- app/admin.py
+++ app/admin.py
@@ -7,3 +7,7 @@ from app.models import Transaction
 admin.site.register(User)
 admin.site.register(Dress)
 admin.site.register(Transaction)
+
+# Not going to bother implementing more complicated admin tools right now
+# For more info, go to Page 2 of the official Django tutorial
+# https://docs.djangoproject.com/en/dev/intro/tutorial02/
--- dressupbox/urls.py
+++ dressupbox/urls.py
@@ -1,17 +1,17 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'dressupbox.views.home', name='home'),
-    # url(r'^dressupbox/', include('dressupbox.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    url(r'^app/$', 'app.views.default'),
+    url(r'^login/$', 'app.views.login'),
+    url(r'^signup/$', 'app.views.signup'),
+    url(r'^map/$', 'app.views.map'),
+    url(r'^dress/$', 'app.views.dress'),
+    url(r'^moreinfo/$', 'app.views.moreinfo'),
+    # url(r'^app/(?P<poll_id>\d+)/$', 'app.views.detail'),
+    # url(r'^app/(?P<poll_id>\d+)/results/$', 'app.views.results'),
+    # url(r'^app/(?P<poll_id>\d+)/vote/$', 'app.views.vote'),
     url(r'^admin/', include(admin.site.urls)),
 )
